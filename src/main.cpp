c1193f0c051f026a564f14c5b72d88cd62ced169
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2015-02-10 20:32:50 +0100

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+
+void rcv(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("exec: ", msg->data.c_str());
+  system(msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "itb_shellcmd");
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("itb_shellcmd_topic", 1000, chatterCallback);
+  ros::spin();
+
+  return 0;
+}
+
+
